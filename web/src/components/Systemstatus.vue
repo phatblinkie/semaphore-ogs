ed3bc1dd3d524de726efe7224373e0112f0cf7a0
phatblinkie <phatblinkie@hotmail.com>
2024-11-26 21:22:56 -0600

    Updated Systemstatus.vue to display disk usage as an HTML5 meter element

--- web/src/components/Systemstatus.vue
+++ web/src/components/Systemstatus.vue
@@ -4,11 +4,15 @@
       <v-toolbar-title> System Status </v-toolbar-title>
     </v-toolbar>
     <v-tabs show-arrows class="pl-4">
-      <v-tab key="Systemstatus" :to="`/project/${projectId}/systemstatus`">System Status</v-tab>
+      <v-tab key="Systemstatus" :to="`/project/${projectId}/systemstatus`">
+        System Status
+      </v-tab>
       <v-tab key="Graphs" :to="`/project/${projectId}/graphs`">Graphs</v-tab>
       <v-tab key="Patchstatus" :to="`/project/${projectId}/patchstatus`">Patch Status</v-tab>
       <v-tab key="Compliancestatus"
-      :to="`/project/${projectId}/compliancestatus`">Compliance Status</v-tab>
+             :to="`/project/${projectId}/compliancestatus`">
+        Compliance Status
+      </v-tab>
     </v-tabs>
 
     <v-data-table
@@ -19,6 +23,19 @@
     >
       <template v-slot:item.ansible_ping="{ item }">
         <TaskStatus :status="item.ansible_ping"/>
+      </template>
+      <template v-slot:item.disk_capacity="{ item }">
+        <div class="disk-meter">
+          <meter
+            :value="item.disk_capacity"
+            :min="0"
+            :max="100"
+            :low="70"
+            :high="90"
+            :optimum="0">
+          </meter>
+          <span>{{ item.disk_capacity }}%</span>
+        </div>
       </template>
     </v-data-table>
   </div>
@@ -76,5 +93,14 @@ export default {
 .left-aligned-table td {
   text-align: left; /* Align text to the left in table headers and cells */
   padding: 8px; /* Optional: add padding to table cells for better spacing */
+}
+.disk-meter {
+  display: flex;
+  align-items: center;
+}
+.disk-meter meter {
+  width: 100%;
+  height: 20px;
+  margin-right: 8px;
 }
 </style>
