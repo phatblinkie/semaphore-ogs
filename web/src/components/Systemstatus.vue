79812a345c4935d5613c8d665492cfc12c935b90
phatblinkie <phatblinkie@hotmail.com>
2025-01-06 21:38:08 -0600

    adding more windows and linux disk cap stuff to status page

--- web/src/components/Systemstatus.vue
+++ web/src/components/Systemstatus.vue
@@ -8,55 +8,62 @@
         System Status
       </v-tab>
       <v-tab key="Graphs" :to="`/project/${projectId}/graphs`">Task Graphs</v-tab>
-      <v-tab key="Patchstatus" :to="`/project/${projectId}/patchstatus`">Patch Status</v-tab>
-      <v-tab key="Compliancestatus"
-             :to="`/project/${projectId}/compliancestatus`">
+      <v-tab key="Patchstatus" :to="`/project/${projectId}/patchstatus`"
+        >Patch Status</v-tab
+      >
+      <v-tab key="Compliancestatus" :to="`/project/${projectId}/compliancestatus`">
         Compliance Status
       </v-tab>
     </v-tabs>
-
     <v-data-table
       :headers="headers"
       :items="items"
       :items-per-page="20"
       :options.sync="tableOptions"
       class="mt-4"
+      dense
     >
       <template v-slot:item.hostname="{ item }">
-        <router-link :to="`/project/${projectId}/host/${item.hostname}`"
-        >{{ item.hostname }}</router-link>
+        <router-link :to="`/project/${projectId}/host/${item.hostname}`">{{
+          item.hostname
+        }}</router-link>
       </template>
       <template v-slot:item.ansible_ping="{ item }">
-        <TaskStatus :status="item.ansible_ping"/>
+        <TaskStatus :status="item.ansible_ping" />
       </template>
       <template v-slot:item.disk_capacity="{ item }">
         <div class="disk-meter">
-          <meter
-            :value="item.disk_capacity"
-            :min="0"
-            :max="100"
-            :low="70"
-            :high="90"
-            :optimum="0">
-          </meter>
-          <span>{{ item.disk_capacity }}%</span>
+          <div v-for="(disk, index) in parseDiskCapacity(item.disk_capacity)" :key="index" class="disk-item">
+            <span class="disk-label">{{ disk.name }}: {{ disk.used }}%</span>
+            <meter
+              :value="disk.used"
+              :min="0"
+              :max="100"
+              :low="70"
+              :high="90"
+              :optimum="0"
+              class="meter"
+            ></meter>
+          </div>
         </div>
       </template>
+
       <template v-slot:item.proc_usage="{ item }">
         <div class="proc-meter">
+          <span class="proc-label">{{ roundProcUsage(item.proc_usage) }}%</span>
           <meter
             :value="roundProcUsage(item.proc_usage)"
             :min="0"
             :max="100"
             :low="70"
             :high="90"
-            :optimum="0">
-          </meter>
-          <span>{{ roundProcUsage(item.proc_usage) }}%</span>
+            :optimum="0"
+            class="meter"
+          ></meter>
         </div>
       </template>
       <template v-slot:item.app_check="{ item }">
-        <AppCheckStatus :status="item.app_check"/>
+        <AppCheckStatus :status="item.app_check" />
       </template>
       <template v-slot:item.last_responded="{ item }">
         <span>{{ formatTimestamp(item.last_responded) }}</span>
@@ -82,13 +89,13 @@ export default {
     return {
       projectId: this.$route.params.projectId, // Extract projectId from route parameters
       headers: [
-        { text: 'Hostname', value: 'hostname' },
-        { text: 'Ansible Ping', value: 'ansible_ping' },
-        { text: 'Disk Usage', value: 'disk_capacity' },
-        { text: 'Proc Usage', value: 'proc_usage' },
-        { text: 'App Check', value: 'app_check' },
-        { text: 'Last Responded', value: 'last_responded' },
-        { text: 'Uptime', value: 'uptime' },
+        { text: 'Hostname', value: 'hostname', width: '120px' },
+        { text: 'Ansible Ping', value: 'ansible_ping', width: '100px' },
+        { text: 'Disk Usage', value: 'disk_capacity', width: 'auto' },
+        { text: 'Proc Usage', value: 'proc_usage', width: 'auto' },
+        { text: 'App Check', value: 'app_check', width: 'auto' },
+        { text: 'Last Responded', value: 'last_responded', width: 'auto' },
+        { text: 'Uptime', value: 'uptime', width: 'auto' },
       ],
       items: [], // This will be populated with data from the JSON source
       tableOptions: {
@@ -132,6 +139,12 @@ export default {
       const hours = Math.floor(uptime / 3600);
       return `${hours}h ${minutes}m ${seconds}s`;
     },
+    parseDiskCapacity(diskCapacity) {
+      return diskCapacity.split(', ').map((disk) => {
+        const [name, used] = disk.split(' ');
+        return { name, used: parseInt(used, 10) };
+      });
+    },
   },
 };
 </script>
@@ -150,13 +163,34 @@ export default {
   text-align: left; /* Align text to the left in table headers and cells */
   padding: 8px; /* Optional: add padding to table cells for better spacing */
 }
-.disk-meter, .proc-meter {
+.disk-meter,
+.proc-meter {
   display: flex;
   align-items: center;
-}
-.disk-meter meter, .proc-meter meter {
   width: 100%;
-  height: 20px;
+}
+.disk-meter {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+}
+.disk-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 4px;
+  width: 100%;
+}
+.disk-label,
+.proc-label {
+  font-weight: bold;
   margin-right: 8px;
+}
+.disk-meter meter,
+.proc-meter meter {
+  flex-grow: 1;
+  height: 20px;
+  width: 100%;
 }
 </style>
